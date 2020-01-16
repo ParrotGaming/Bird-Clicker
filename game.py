a46133575a0b875721af9da14584f2e8cc5b2d48
ParrotGaming <jacobparrotoffical@gmail.com>
2020-01-15 19:41:34 -0800

    continued work on port

--- game.py
+++ game.py
@@ -10,6 +10,10 @@ f1 = pygame.image.load('./assets/f1.png')
 f2 = pygame.image.load('./assets/f2.png')
 icon = pygame.image.load('./assets/Icon.png')
 icons = pygame.transform.scale(icon, (250,250))
+breeder = pygame.image.load('./assets/breeder.png')
+breeders = pygame.transform.scale(breeder, (250,250))
+
+breeder_points = 0
 
 points = 0
 
@@ -25,9 +29,25 @@ def print_points():
     WintextRect.center = (100, 250)
     win.blit(WinDraw, WintextRect)
 
+def print_bpoints():
+    font = pygame.font.Font('./assets/LCD_Solid.ttf', 32)
+    WinDraw2 = font.render(f"{breeder_points}", True, white, black)
+    WintextRect2 = WinDraw2.get_rect()
+    WintextRect2.center = (250, 400)
+    win.blit(WinDraw2, WintextRect2)
+
+def print_points():
+    font = pygame.font.Font('./assets/LCD_Solid.ttf', 32)
+    WinDraw = font.render(f"{points}", True, white, black)
+    WintextRect = WinDraw.get_rect()
+    WintextRect.center = (100, 250)
+    win.blit(WinDraw, WintextRect)
+
 pos = 0
 
 timer = 0
+
+timer2 = 0
 
 height = 150
 
@@ -36,23 +56,34 @@ while True:
 
     win.blit(background,(0,0))
     win.blit(icons,(125,height))
+    win.blit(breeders, (135,250))
 
     for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.display.quit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_SPACE:
                         height = 125
                         points += 1
+                    if event.key == pygame.K_RETURN:
+                        if points >= 10:
+                            breeder_points += 1
+                            points -= 10
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_SPACE:
                         height = 150
 
     if timer >= 10:
         pos += 1
         timer = 0
 
+    if timer2 >= 15:
+        points += 1 * breeder_points
+        timer2 = 0
+
     timer += 1
+
+    timer2 += 1
 
     if pos == 0:
         background = f1
@@ -65,5 +96,6 @@ while True:
         timer = 0
 
     print_points()
+    print_bpoints()
 
     pygame.display.update()
