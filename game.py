31a74fbe06b4ced8b45da2acad3f50e635671d2e
Killian Seller <kseller@e1z2r3p33.42.us.org>
2020-01-18 14:58:00 -0800

    almost finished port

--- game.py
+++ game.py
@@ -12,6 +12,12 @@ icon = pygame.image.load('./assets/Icon.png')
 icons = pygame.transform.scale(icon, (250,250))
 breeder = pygame.image.load('./assets/breeder.png')
 breeders = pygame.transform.scale(breeder, (250,250))
+aicon = pygame.image.load('./assets/AVI2.png')
+aicons = pygame.transform.scale(aicon, (125,125))
+
+avery_points = 0
+avery_cost = 100
+a_increment = 1
 
 breeder_points = 0
 breeder_cost = 10
@@ -27,24 +33,25 @@ black = (0, 0, 0)
 
 def print_points():
     font = pygame.font.Font('./assets/LCD_Solid.ttf', 32)
-    WinDraw = font.render(f"{int(points)}", True, white, black)
+    WinDraw = font.render(f"{int(points)}", True, white, (0, 183, 255))
     WintextRect = WinDraw.get_rect()
     WintextRect.center = (100, 250)
     win.blit(WinDraw, WintextRect)
 
 def print_bpoints():
     font = pygame.font.Font('./assets/LCD_Solid.ttf', 32)
-    WinDraw2 = font.render(f"{breeder_points}", True, white, black)
+    WinDraw2 = font.render(f"{breeder_points}", True, white, (0, 183, 255))
     WintextRect2 = WinDraw2.get_rect()
-    WintextRect2.center = (250, 400)
+    WintextRect2.center = (350, 365)
     win.blit(WinDraw2, WintextRect2)
 
-def print_points():
+def print_apoints():
     font = pygame.font.Font('./assets/LCD_Solid.ttf', 32)
-    WinDraw = font.render(f"{points}", True, white, black)
-    WintextRect = WinDraw.get_rect()
-    WintextRect.center = (100, 250)
-    win.blit(WinDraw, WintextRect)
+    WinDraw3 = font.render(f"{avery_points}", True, white, (0, 183, 255))
+    WintextRect3 = WinDraw3.get_rect()
+    WintextRect3.center = (320, 435)
+    win.blit(WinDraw3, WintextRect3)
+
 
 pos = 0
 
@@ -60,6 +67,7 @@ while True:
     win.blit(background,(0,0))
     win.blit(icons,(125,height))
     win.blit(breeders, (135,250))
+    win.blit(aicons, (190,375))
 
     for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -70,6 +78,10 @@ while True:
                             breeder_points += 1
                             points -= breeder_cost
                         print(breeder_cost)
+                    if event.key == pygame.K_RSHIFT:
+                        if points >= avery_cost:
+                            avery_points += 1
+                            points -= avery_cost
                 if pygame.mouse.get_pressed()[0]:
                     points += 1
                     height = 125
@@ -85,12 +97,18 @@ while True:
 
     if timer2 >= 15:
         points += 1 * breeder_points
+        points += 10 * avery_points
         timer2 = 0
 
     if breeder_points > 0:
         increment = breeder_points * 1.5
 
     breeder_cost = 10 * round(increment)
+
+    if avery_points > 0:
+        a_increment = avery_points * 1.5
+
+    avery_cost = 100 * round(a_increment)
 
     timer += 1
 
@@ -108,5 +126,6 @@ while True:
 
     print_points()
     print_bpoints()
+    print_apoints()
 
     pygame.display.update()
